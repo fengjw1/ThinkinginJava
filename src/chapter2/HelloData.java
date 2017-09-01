b05a919bb7462972f4fb1e6bbc2a2ce1da35d588
fengjw <fengjwxx@163.com>
2017-09-01 22:58:27 +0800

    today commit first

---
+++ src/chapter2/HelloData.java
@@ -0,0 +1,15 @@
+package chapter2;
+
+import java.util.Date;
+
+/**
+ * Created by fengjw on 2017/9/1
+ * Code Change The World!
+ */
+
+public class HelloData {
+    public static void main(String [] args){
+        System.out.println("Hello, it's");
+        System.out.println(new Date());
+    }
+}
