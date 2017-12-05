e800147763306903320552cb43efa71850f9186b
fengjw <fengjwxx@163.com>
2017-12-05 21:42:17 +0800

    作业第二次提交

--- src/exercise/Point2D.java
+++ src/exercise/Point2D.java
@@ -1,5 +1,7 @@
 package exercise;
 
+import java.text.DecimalFormat;
+
 /**
  * Created by fengjw on 2017/12/5
  * Code Change The World!
@@ -31,15 +33,25 @@ public class Point2D {
     }
 
     public void setPos(double x, double y){
-
+        setmX(x);
+        setmY(y);
     }
 
-    public void calculateLength(){
-
+    public double calculateLength(){
+        DecimalFormat df = new DecimalFormat("#.##");
+        double result = Double.parseDouble(df.format(Math.sqrt(mX * mX + mY * mY)));
+        return result;
     }
 
-    public void distanceTo(Point2D point2D){
 
+    public double distanceTo(Point2D point2D){
+        DecimalFormat df = new DecimalFormat("#.##");
+        double userX = point2D.getmX();
+        double userY = point2D.getmY();
+        double lengthX = Math.abs(userX - mX);
+        double lengthY = Math.abs(userY - mY);
+        double result = Double.parseDouble(df.format(Math.sqrt(lengthX * lengthX + lengthY * lengthY)));
+        return result;
     }
 
 
--- src/exercise/VectorsCsv.java
+++ src/exercise/VectorsCsv.java
@@ -14,8 +14,13 @@ public class VectorsCsv {
 
     private static Scanner stream;
     private static ArrayList<Point2D> list;
+    private static ArrayList<Point2D> tempData;
+    private static double[] tempOriginData;
+    private static double[] tempUserData;
 
     public static void main(String [] args){
+
+        ArrayList<Point2D> vectors = new ArrayList<Point2D>();
 
         try {
             File file = new File("D:test.csv");
@@ -26,12 +31,59 @@ public class VectorsCsv {
         }
 
         readFromFile(stream, list);
+        inputPoint2D();
 
-        for (int i = 0; i < list.size(); i ++){
-            System.out.println(list.get(i).getmName());
-            System.out.println(list.get(i).getmX());
-            System.out.println(list.get(i).getmY());
+        for (int i = 0; i < tempOriginData.length; i ++){
+            System.out.println(tempOriginData[i]);
         }
+
+        for (int i = 0; i < tempUserData.length; i ++){
+            System.out.println(tempUserData[i]);
+        }
+    }
+
+    /**
+     * calculate value to user point(x,y)
+     */
+    private static void getToUserLength(){
+        tempUserData = new double[5];
+        Point2D point2D;
+        for (int i = 0; i < list.size(); i ++){
+            point2D = tempData.get(i);
+            tempUserData[i] = list.get(i).distanceTo(point2D);
+        }
+    }
+
+    /**
+     * calculate value to origin
+     */
+    private static void getCalculateLength(){
+        tempOriginData = new double[5];
+        for (int i = 0; i < list.size(); i ++){
+            tempOriginData[i] = list.get(i).calculateLength();
+        }
+    }
+
+    /**
+     * input name,x,y
+     */
+    private static void inputPoint2D(){
+        tempData = new ArrayList<>();
+        for (int i = 0; i < 5; i ++) {
+            Point2D point2D = new Point2D();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please input Name : ");
+            String name = scanner.nextLine();
+            System.out.println("Please input X : ");
+            double x = Double.parseDouble(scanner.nextLine());
+            System.out.println("Please input y : ");
+            double y = Double.parseDouble(scanner.nextLine());
+            point2D.setmName(name);
+            point2D.setPos(x, y);
+            tempData.add(point2D);
+        }
+        getToUserLength();
+        getCalculateLength();
     }
 
     /**
